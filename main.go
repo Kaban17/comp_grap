eebe3bb546507291cad5d477b0e3cf42fe00da99
Kaban17 <kaban1628@gmail.com>
2025-04-07 22:08:14 +0400

    rotate model

--- main.go
+++ main.go
@@ -10,10 +10,9 @@ import (
 
 func main() {
 	const (
-		aX = 2000.0
-		aY = 2000.0
-		u0 = 250.0
-		v0 = 250.0
+		scale   = 2500.0
+		offsetX = 250.0
+		offsetY = 250.0
 	)
 
 	angleX := math.Pi / 6
@@ -26,7 +25,7 @@ func main() {
 
 	rotation := rotationX.Multiply(rotationY.Multiply(rotationZ))
 
-	tx, ty, tz := 0.0, 0.0, 10.0
+	tx, ty, tz := 0.0, 0.0, 0.0
 
 	mat := geometry.NewMatrix(500, 500, true, geometry.RGBColor{R: 255, G: 255, B: 255})
 	zb := geometry.NewZBuffer(500, 500)
@@ -41,64 +40,36 @@ func main() {
 		if len(face.Indices) < 3 {
 			continue
 		}
-		vert0 := vertex[face.Indices[0]-1]
-		vert1 := vertex[face.Indices[1]-1]
-		vert2 := vertex[face.Indices[2]-1]
+		v0 := vertex[face.Indices[0]-1]
+		v1 := vertex[face.Indices[1]-1]
+		v2 := vertex[face.Indices[2]-1]
 
-		p0 := geometry.Point3D{
-			X: float64(vert0.X),
-			Y: float64(vert0.Y),
-			Z: float64(vert0.Z),
-		}
-		p1 := geometry.Point3D{
-			X: float64(vert1.X),
-			Y: float64(vert1.Y),
-			Z: float64(vert1.Z),
-		}
-		p2 := geometry.Point3D{
-			X: float64(vert2.X),
-			Y: float64(vert2.Y),
-			Z: float64(vert2.Z),
-		}
+		p0 := geometry.Point3D{X: v0.X, Y: v0.Y, Z: v0.Z}
+		p1 := geometry.Point3D{X: v1.X, Y: v1.Y, Z: v1.Z}
+		p2 := geometry.Point3D{X: v2.X, Y: v2.Y, Z: v2.Z}
 
 		p0 = rotation.TransformPoint(p0)
 		p1 = rotation.TransformPoint(p1)
 		p2 = rotation.TransformPoint(p2)
 
-		p0.X += tx
-		p0.Y += ty
-		p0.Z += tz
-		p1.X += tx
-		p1.Y += ty
-		p1.Z += tz
-		p2.X += tx
-		p2.Y += ty
-		p2.Z += tz
+		p0 = geometry.Translate(p0, tx, ty, tz)
+		p1 = geometry.Translate(p1, tx, ty, tz)
+		p2 = geometry.Translate(p2, tx, ty, tz)
 
 		tri3D := geometry.TriangleVertices3D{
-			P1: p0,
-			P2: p1,
-			P3: p2,
+			P1: geometry.Point3D{X: p0.X, Y: p0.Y, Z: p0.Z},
+			P2: geometry.Point3D{X: p1.X, Y: p1.Y, Z: p1.Z},
+			P3: geometry.Point3D{X: p2.X, Y: p2.Y, Z: p2.Z},
 		}
 
 		normal := tri3D.CalculateNormal()
 		cosTheta := normal.Z // Направление света [0,0,1]
 
 		if cosTheta < 0 {
-
 			tri2D := geometry.TriangleVertices2D{
-				P1: geometry.Point2D{
-					X: p0.X*aX + u0,
-					Y: -p0.Y*aY + v0,
-				},
-				P2: geometry.Point2D{
-					X: p1.X*aX + u0,
-					Y: -p1.Y*aY + v0,
-				},
-				P3: geometry.Point2D{
-					X: p2.X*aX + u0,
-					Y: -p2.Y*aY + v0,
-				},
+				P1: geometry.Point2D{X: p0.X*scale + offsetX, Y: -p0.Y*scale + offsetY},
+				P2: geometry.Point2D{X: p1.X*scale + offsetX, Y: -p1.Y*scale + offsetY},
+				P3: geometry.Point2D{X: p2.X*scale + offsetX, Y: -p2.Y*scale + offsetY},
 				Color: geometry.RGBColor{
 					R: uint8(-255 * cosTheta),
 					G: 0,
@@ -111,7 +82,7 @@ func main() {
 	}
 
 	img := save.MatrixToImage(&mat)
-	if err := save.SaveImage(img, "output1.png"); err != nil {
+	if err := save.SaveImage(img, "output.png"); err != nil {
 		fmt.Println("Error saving image:", err)
 	}
 }
